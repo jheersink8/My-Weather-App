d03106f4b3f92f938b14ff3042f4ad92343b21c7
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-01-30 22:30:13 -0700

    Adding daily data minus icons.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,14 @@
+var currentCity = $("#currentCity");
+var currentDate = $("#currentDate");
+var currentTemp = $("#currentTemp");
+var currentHumid = $("#currentHumid");
+var currentWind = $("#currentWind");
+var currentIcon = $("#currentIcon");
+
+
+
+
+
 var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=39.74&lon=-104.99&units=imperial&appid=0033aad0c09d93beb4a60c3cfe05890e"
 
 fetch(requestURL)
@@ -6,12 +17,12 @@ fetch(requestURL)
     })
     .then(function (data) {
         console.log(data);
-        for (var i = 0; i < data.list.length; i++) {
-            console.log("City: "+data.city.name+", "+ data.city.country)
-            console.log("Date & Time: "+data.list[i].dt_txt)
-            console.log("Wind Speed: "+data.list[i].wind.speed)
-            console.log("Humidity: "+data.list[i].main.humidity)
-            console.log("Temp: "+data.list[i].main.temp)
+        {
+            currentCity.text("City: "+data.city.name+", "+ data.city.country)
+            currentDate.text("Date & Time: "+data.list[1].dt_txt)
+            currentWind.text("Wind Speed: "+data.list[1].wind.speed)
+            currentHumid.text("Humidity: "+data.list[1].main.humidity)
+            currentTemp.text("Temp: "+data.list[1].main.temp)
         }
-
+        // for (var i = 0; i < data.list.length; i++) 
     }); 
