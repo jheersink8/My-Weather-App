4580d84485d39afe686ce4f29878f4399e3501c3
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-02-03 14:52:56 -0700

    Minor clean up before starting. This is a good fallback commit.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,13 +6,12 @@ var currentHumid = $("#currentHumid");
 var currentWind = $("#currentWind");
 var currentIcon = $("#currentIcon");
 var search = $("#search");
+var searchResult = $("#searchResult");
 var requestURLCurrent;
 var requestURLFiveDay;
 
 // Function to convert city name to lat/lon coordinates.//
-var coordinatesConvert = function (event) {
-    event.preventDefault();
-    citySearch = "http://api.openweathermap.org/geo/1.0/direct?q=" + $("#searchResult").val() + "&limit=1&appid=0033aad0c09d93beb4a60c3cfe05890e"
+var coordinatesConvert = function () {
     fetch(citySearch, {
         cache: "no-cache",
     })
@@ -30,15 +29,18 @@ var coordinatesConvert = function (event) {
 
 // Function with fetch request to search lat/lon coordinates and output API results.//
 var coordinatesSearchCurrent = function () {
-    fetch(requestURLCurrent)
+    fetch(requestURLCurrent, {
+        cache: "no-cache",
+    })
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            console.log(data)
             currentDate.text("Date: " + dayjs.unix(data.dt).format("MMMM D, YYYY"));
-            currentTemp.text("Temp: " + data.main.temp + "°");
-            currentHumid.text("Humidity: " + data.main.humidity + "%");
-            currentWind.text("Wind Speed: " + data.wind.speed + " mph");
+            currentTemp.text("Temp: " + Math.round(data.main.temp) + "°");
+            currentHumid.text("Humidity: " + Math.round(data.main.humidity) + "%");
+            currentWind.text("Wind Speed: " + Math.round(data.wind.speed) + " mph");
             currentIcon.attr("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
         })
 };
@@ -56,22 +58,30 @@ var coordinatesSearchFiveDay = function () {
             var time1 = dayjs(data.list[0].dt_txt);
             var time2 = ((time1.add(12, "hour")).startOf('day').add(12, "hour"));
             var timeCode = Math.floor(time2.diff(time1, "hours") / 3);
-            // DELETE//////////////////////////////////////////////////////////////////////////////////
-            console.log(timeCode);
-            console.log(data);
             // For loop to grab data for each day at noon.//
             var dayStart = 0;
             for (var i = timeCode; i < data.list.length; i += 8) {
                 var day = "#day-" + dayStart;
-                $(day).children().eq(0).text("Date: " + data.list[i].dt_txt);
-                $(day).children().eq(1).text("Temp: " + data.list[i].main.temp);
+                $(day).children().eq(0).text(dayjs(data.list[i].dt_txt).format("ddd MMM, D"));
+                $(day).children().eq(1).text(Math.round(data.list[i].main.temp) + "°");
                 $(day).children().eq(2).attr("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png")
-                $(day).children().eq(3).children().eq(0).text("Humidity: " + data.list[i].main.humidity);
-                $(day).children().eq(3).children().eq(1).text("Wind Speed: " + data.list[i].wind.speed);
+                $(day).children().eq(3).children().eq(0).text(Math.round(data.list[i].main.humidity) + "%");
+                $(day).children().eq(3).children().eq(1).text(Math.round(data.list[i].wind.speed) + " mph");
                 dayStart++
             }
         })
 };
 
 // Form submit search event listener//
-search.on("submit", coordinatesConvert);
+search.on("submit", function (event) {
+    event.preventDefault();
+    citySearch = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchResult.val() + "&limit=1&appid=0033aad0c09d93beb4a60c3cfe05890e"
+    coordinatesConvert();
+});
+
+// test.on("click", function (){
+//     var favoriteSearch = "alamosa"; 
+//     citySearch = "http://api.openweathermap.org/geo/1.0/direct?q=" + favoriteSearch + "&limit=1&appid=0033aad0c09d93beb4a60c3cfe05890e"
+//     console.log(citySearch)
+//     coordinatesConvert();
+// }) ; 
