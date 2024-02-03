bdb7292ca27eddbc0f7b5b9d0d05c3f421e90400
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-02-02 23:59:50 -0700

    Figured out noon output. Cleaning code before merge.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,8 +8,6 @@ var search = $("#search");
 var requestURLCurrent;
 var requestURLFiveDay;
 
-
-
 // Function to convert city name to lat/lon coordinates.//
 var coordinatesConvert = function (event) {
     event.preventDefault();
@@ -20,7 +18,6 @@ var coordinatesConvert = function (event) {
         })
         .then(function (data) {
             currentCity.text("City: " + data[0].name + ", " + data[0].state)
-            console.log(data)
             requestURLCurrent = "https://api.openweathermap.org/data/2.5/weather?lat=" + (data[0].lat) + "&lon=" + (data[0].lon) + "&units=imperial&appid=0033aad0c09d93beb4a60c3cfe05890e"
             coordinatesSearchCurrent(requestURLCurrent);
             requestURLFiveDay = "https://api.openweathermap.org/data/2.5/forecast?lat=" + (data[0].lat) + "&lon=" + (data[0].lon) + "&units=imperial&appid=0033aad0c09d93beb4a60c3cfe05890e"
@@ -35,7 +32,6 @@ var coordinatesSearchCurrent = function () {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
             currentDate.text("Date: " + dayjs.unix(data.dt).format("MMMM D, YYYY"));
             currentTemp.text("Temp: " + data.main.temp + "°");
             currentHumid.text("Humidity: " + data.main.humidity + "%");
@@ -52,14 +48,25 @@ var coordinatesSearchFiveDay = function () {
         })
         .then(function (data) {
             // For loop to grab data for each day at noon.//
-            console.log(data);
-            for (var i = 0; i < data.list.length; i++) {
-                var day = "#day-" + i;
+            var dayStart = 0;
+            // Calculations to output a value of noon on the page (more details in ReadMe)//
+            var time1 = dayjs().startOf('day');
+            var time2 = dayjs();
+            var timeDiffCode = Math.floor(time2.diff(time1, "hours") / 3);
+            var timeCode;
+            if (timeDiffCode !== 8) {
+                timeCode = 8 - timeDiffCode
+            } else {
+                timeCode = 0
+            };
+            for (var i = timeCode; i < data.list.length; i += 8) {
+                var day = "#day-" + dayStart;
                 $(day).children().eq(0).text("Date: " + data.list[i].dt_txt);
                 $(day).children().eq(1).text("Temp: " + data.list[i].main.temp);
                 $(day).children().eq(2).attr("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png")
                 $(day).children().eq(3).children().eq(0).text("Humidity: " + data.list[i].main.humidity);
                 $(day).children().eq(3).children().eq(1).text("Wind Speed: " + data.list[i].wind.speed);
+                dayStart++
             }
         })
 };
@@ -69,15 +76,3 @@ search.on("submit", coordinatesConvert);
 
 
 
-
-// "$("#date1");
-// "$("#temp1");
-// "$("#icon1");
-// "$("#hum1");
-// "$("#wind1");
-
-// $("#date1").text("Date: " + data.list[i].dt_txt)
-// console.log("Date & Time: " + data.list[i].dt_txt)
-// console.log("Temp: " + data.list[i].main.temp)
-// console.log("Humidity: " + data.list[i].main.humidity)
-// console.log("Wind Speed: " + data.list[i].wind.speed)
